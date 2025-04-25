c56c816a2020ee54fe9a9c2cfd19c081d8c800e7
Sonali <sonalii04dec@gmail.com>
2025-04-26 01:39:33 +0530

    msg

--- quiz/admin.py
+++ quiz/admin.py
@@ -6,6 +6,15 @@ from django.contrib import admin
 from .models import Quiz, Question, Answer, UserQuizAttempt
 
 admin.site.register(UserQuizAttempt)
-admin.site.register(Question)
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Answer)
 admin.site.register(Quiz)
